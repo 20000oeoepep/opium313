39bbbfc80abc4a1164e3ca48a3eab60a9e8b6c1e
20000oeoepep <djdksks313@gmail.com>
2025-07-13 21:51:54 +0300

    Update virgin.js

--- Priyansh/commands/virgin.js
+++ Priyansh/commands/virgin.js
@@ -1,25 +1,30 @@
 module.exports.config = {
-	name: "virgin",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Random picture of the most beautiful boy in Vietnam :))",
-	commandCategory: "Random-IMG",
-	usages: "virgin",
-	cooldowns: 5
+    name: "oneTwoResponder",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "سواد البغدادي",
+    description: "رد تلقائي على 1 و 2",
+    commandCategory: "no prefix",
+    usages: "1 أو 2",
+    cooldowns: 2,
 };
 
-module.exports.run = async ({ api, event }) => {
-	const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-	axios.get('https://ngoctrinh.ocvat2810.repl.co/').then(res => {
-	let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
-	let callback = function () {
-					api.sendMessage({
-						attachment: fs.createReadStream(__dirname + `/cache/trinh.${ext}`)
-					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/trinh.${ext}`), event.messageID);
-				};
-				request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/trinh.${ext}`)).on("close", callback);
-			})
-}
+module.exports.handleEvent = function({ api, event }) {
+    const { threadID, messageID, body } = event;
+
+    if (!body) return;
+
+    const text = body.trim();
+
+    if (text === "1") {
+        api.sendMessage("الحمدلله", threadID, messageID);
+    }
+
+    if (text === "2") {
+        api.sendMessage("دائماً وأبداً", threadID, messageID);
+    }
+};
+
+module.exports.run = function({ api, event }) {
+    // هذا الأمر لا يستخدم أمر مباشر، فقط من خلال handleEvent
+};
